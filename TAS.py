e9c333d5f0c9516ffd9b14fabc9901f33ee4b969
Easton Zou <eastonzou@gmail.com>
2025-11-16 19:22:16 -0500

    e

--- TAS.py
+++ TAS.py
@@ -1,9 +1,4 @@
 import pyautogui
-import time
-pyautogui.click()
-time.sleep(0.53)
-pyautogui.click()
-time.sleep(0.45)
-pyautogui.click()
-time.sleep(0.41)
-pyautogui.click()
+for i in range(100):
+    pyautogui.hotkey("ctrl","v")
+    pyautogui.press("enter")
